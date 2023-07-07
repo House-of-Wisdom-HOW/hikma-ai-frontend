8ed9d02c1c726d72c6eac2281189d4a67a22bd9e
Hassan Chowdhry <94680098+HassanChowdhry@users.noreply.github.com>
2023-07-08 03:39:46 +0500

    fixed grid resposinve issue and added dummy data to json file

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -9,10 +9,10 @@ interface Props {
 
 function Article({ src, alt, title, description }: Props) {
   return (
-    <Grid item xs={3}>
+    <Grid item xs={12} sm={6} md={4} lg={3}>
       <Card
         sx={{ backgroundColor: '#FAFAFA', boxShadow: '2px 3px 4px #e1b15d80' }}
-        className='text-center h-full w-9/12  mx-auto'
+        className='text-center h-full w-4/5  mx-auto'
       >
         <CardActionArea
           sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start' }}
--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,57 +1,7 @@
 import { Typography, Box, Grid } from '@mui/material';
 import SortIcon from '@mui/icons-material/Sort';
 import Article from '../components/Article';
-import drippi from '../pics/DrippiAi.png';
-import magicast from '../pics/MagicastAi.png';
-import poster from '../pics/PosterStudio.png';
-import codeConv from '../pics/codeConv.png';
-import vribble from '../pics/Vribble.png';
-import QR from '../pics/QR.png';
-
-const DUMMY_DATA = [
-  {
-    src: drippi,
-    alt: 'Drippi.ai',
-    title: 'Drippi.ai',
-    description: 'A tool to automate personalized Twitter outreach.',
-    id: 1
-  },
-  {
-    src: magicast,
-    alt: 'Magicast.ai',
-    title: 'Magicast.ai',
-    description: 'A platform to generate personalized podcasts.',
-    id: 2
-  },
-  {
-    src: poster,
-    alt: 'Poster Studio',
-    title: 'Poster Studio',
-    description: 'A tool to generate ads and social creatives.',
-    id: 3
-  },
-  {
-    src: QR,
-    alt: 'QR-Art',
-    title: 'QR-Art',
-    description: 'A tool to generate customizable QR codes from images.',
-    id: 4
-  },
-  {
-    src: vribble,
-    alt: 'VribbleAI',
-    title: 'VribbleAI',
-    description: 'A tool to capture, store, and organize your thoughts and ideas.',
-    id: 5
-  },
-  {
-    src: codeConv,
-    alt: 'AI Code Converter',
-    title: 'AI Code Converter',
-    description: 'A tool to generate code and convert code from one programming language to another.',
-    id: 6
-  }
-];
+import DUMMY_DATA from '../data.json';
 
 function Main() {
   return (
@@ -59,12 +9,11 @@ function Main() {
       <Box className='flex justify-between'>
         <Typography variant='h6'>Showing 3 of 100 Total Tools.</Typography>
         <Typography>
-          Sort by: Recent
-          <SortIcon />
+          Sort by: Recent <SortIcon />
         </Typography>
       </Box>
 
-      <Grid container className='mt-10' rowGap={3} >
+      <Grid container className='mt-10' rowGap={6} >
         {DUMMY_DATA.map(({ src, alt, title, description, id }) => (
           <Article src={src} alt={alt} title={title} description={description} key={id}/>
         ))}
