5569a915055ab8d75c22397b2db855ae0b5ff888
Hassan Chowdhry <94680098+HassanChowdhry@users.noreply.github.com>
2023-07-18 02:43:47 +0500

    loading spinner

--- src/layout/Add.tsx
+++ src/layout/Add.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { useState, ChangeEvent, FormEvent } from 'react';
 import { Box, Typography, FormGroup, Button } from '@mui/material';
 import FilterCheckbox from '../components/FilterCheckbox';
+import LoadingSpinner from '../components/LoadingSpinner';
 import tags from '../data/tags.json';
 import emailjs from 'emailjs-com';
 import ToolInput from '../components/ToolInput';
@@ -17,6 +19,7 @@ interface FormValues {
 
 function Add() {
   const formTags = [...tags, { tag: 'Other', id: tags.length + 1 }];
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [formValues, setFormValues] = useState<FormValues>({
     name: '',
     email: '',
@@ -27,6 +30,7 @@ function Add() {
     otherTag: ''
   });
 
+  // email message template
   const emailTemplate = `
     Name: ${formValues.name}
     Email: ${formValues.email}
@@ -47,7 +51,7 @@ function Add() {
   };
 
   // adds checked tags
-  const handleSubmit = (e: FormEvent) => {
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     // Use the formValues object to send the email using EmailJS
     const emailParams = {
@@ -55,21 +59,35 @@ function Add() {
       from_email: formValues.email,
       message: emailTemplate
     };
-    emailjs
-      .send(
-        process.env.REACT_APP_SERVICE_ID ?? '',
-        process.env.REACT_APP_TEMPLATE_ID ?? '',
-        emailParams,
-        process.env.REACT_APP_PUBLIC_KEY ?? '',
-      )
-      .then((response) => {
-        console.log('Email sent successfully!', response);
-      })
-      .catch((error) => {
-        console.error('Error sending email:', error);
+    setIsLoading(true);
+    try {
+      await emailjs
+        .send(
+          process.env.REACT_APP_SERVICE_ID ?? '',
+          process.env.REACT_APP_TEMPLATE_ID ?? '',
+          emailParams,
+          process.env.REACT_APP_PUBLIC_KEY ?? '',
+        )
+        .then((response) => {
+          console.log('Email sent successfully!', response);
+        });
+      // set back to default
+      setFormValues({
+        name: '',
+        email: '',
+        toolName: '',
+        toolURL: '',
+        description: '',
+        tags: [],
+        otherTag: ''
       });
+    } catch (error) {
+      console.error('Error sending email:', error);
+    }
+    setIsLoading(false);
   };
 
+  // adds tag to list in form values
   const handleTagChange = (tag: string) => {
     if (formValues.tags.includes(tag)) {
       setFormValues((prevValues) => ({
@@ -88,79 +106,87 @@ function Add() {
 
   return (
     <Box component='form' className='p-8 w-11/12 sm:max-w-lg h-auto bg-gray m-auto text-center shadow-bown' style={{ marginTop: '50px' }}>
-      <Typography variant='h5'>Add a Tool</Typography>
 
-      <ToolInput
-        id='name'
-        label='Your Name'
-        value={formValues.name}
-        handleChange={handleChange}
-      />
-      <ToolInput
-        id='email'
-        label='Your Email'
-        value={formValues.email}
-        handleChange={handleChange}
-      />
+      {isLoading && <LoadingSpinner/>}
 
-      <ToolInput
-        id='toolName'
-        label='Tool Name'
-        value={formValues.toolName}
-        handleChange={handleChange}
-      />
-      <ToolInput
-        id='toolURL'
-        label='Tool URL'
-        value={formValues.toolURL}
-        handleChange={handleChange}
-      />
+      {!isLoading && (
+        <>
+          <Typography variant='h5'>Add a Tool</Typography>
 
-      <ToolInput
-        id='description'
-        label='Description'
-        value={formValues.description}
-        handleChange={handleChange}
-        multiline
-        maxRows={3}
-      />
-
-      <Typography variant='h6'>Tool Tags</Typography>
-      <FormGroup
-        className='mt-2 gap-3'
-        sx={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(2, 1fr)',
-          '@media (min-width: 900px)': {
-            gridTemplateColumns: 'repeat(3, 1fr)',
-          },
-        }}>
-        {formTags.map(({ tag, id }) => (
-          <FilterCheckbox
-            label={tag}
-            key={id}
-            checked={formValues.tags.includes(tag)}
-            onTagSelect={() => { handleTagChange(tag); }}
+          {/* Name, Email, tool name and URL input fields */}
+          <ToolInput
+            id='name'
+            label='Your Name'
+            value={formValues.name}
+            handleChange={handleChange}
           />
-        ))}
-      </FormGroup>
+          <ToolInput
+            id='email'
+            label='Your Email'
+            value={formValues.email}
+            handleChange={handleChange}
+          />
+          <ToolInput
+            id='toolName'
+            label='Tool Name'
+            value={formValues.toolName}
+            handleChange={handleChange}
+          />
+          <ToolInput
+            id='toolURL'
+            label='Tool URL'
+            value={formValues.toolURL}
+            handleChange={handleChange}
+          />
+          <ToolInput
+            id='description'
+            label='Description'
+            value={formValues.description}
+            handleChange={handleChange}
+            multiline
+            maxRows={3}
+          />
 
-      <ToolInput
-        id='otherTag'
-        label='Other'
-        value={formValues.otherTag}
-        handleChange={handleChange}
-        disabled={!formValues.tags.includes('Other')}
-      />
+          {/* check boxes */}
+          <Typography variant='h6'>Tool Tags</Typography>
+          <FormGroup
+            className='mt-2 gap-3'
+            sx={{
+              display: 'grid',
+              gridTemplateColumns: 'repeat(2, 1fr)',
+              '@media (min-width: 900px)': {
+                gridTemplateColumns: 'repeat(3, 1fr)',
+              },
+            }}>
+            {formTags.map(({ tag, id }) => (
+              <FilterCheckbox
+                label={tag}
+                key={id}
+                checked={formValues.tags.includes(tag)}
+                onTagSelect={() => { handleTagChange(tag); }}
+              />
+            ))}
+          </FormGroup>
 
-      <Button
-        sx={{ marginTop: '20px', color: 'white', width: '100%' }}
-        variant='contained'
-        color='primary'
-        onClick={handleSubmit}
-      >
-        Submit
-      </Button>
+          {/* Other input field */}
+          <ToolInput
+            id='otherTag'
+            label='Other'
+            value={formValues.otherTag}
+            handleChange={handleChange}
+            disabled={!formValues.tags.includes('Other')}
+          />
+
+          <Button
+            sx={{ marginTop: '20px', color: 'white', width: '100%' }}
+            variant='contained'
+            color='primary'
+            onClick={handleSubmit}
+          >
+            Submit
+          </Button>
+        </>
+      )}
     </Box>
   );
 }
